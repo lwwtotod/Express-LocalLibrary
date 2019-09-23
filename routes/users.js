8fe794e7e7524338872b3b133ed0a61718b95f09
shunshun <lwwtotod@gmail.com>
2019-09-23 14:29:09 +0800

    feat:创建控制器层和路由访问

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+  res.send('respond with a resource')
+})
 router.get('/cool', function(req, res, next) {
-  res.send('你好酷');
-});
-module.exports = router;
+  res.send('你好酷')
+})
+module.exports = router
